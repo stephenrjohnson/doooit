b44ef7eab1fa2e24f6427393b642494d640d2f96
Stephen Johnson <stephen.johnson@ahc.uk.com>
2012-04-26 14:37:43 +0100

    fix error message

--- lib/settings.rb
+++ lib/settings.rb
@@ -6,7 +6,7 @@ class Settings < Settingslogic
 	    elsif File.exist?(File.expand_path("../config/doit"))
 	    	@@config = File.expand_path("../config/doit")  
 	    else
-	      puts "There's no configuration! please create one at ~/doit"
+	      puts "There's no configuration! please create one at ~/.doit"
 	      exit!
 	  	end
 	    source @@config
