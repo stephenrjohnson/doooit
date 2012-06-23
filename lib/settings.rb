9790d6e8c69a700ffeb7f53f9044d218a8e00ef6
Stephen <steve@thatbytes.co.uk>
2012-06-23 23:46:40 +0100

    Fix name of config file

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,12 +1,12 @@
 require 'settingslogic'
 class Settings < Settingslogic
 	def self.load
-		if File.exist?(File.expand_path("~/.doit"))
-	    	@@config = File.expand_path("~/.doit")
-	    elsif File.exist?(File.expand_path("../config/doit"))
-	    	@@config = File.expand_path("../config/doit")  
+		if File.exist?(File.expand_path("~/.doooit"))
+	    	@@config = File.expand_path("~/.doooit")
+	    elsif File.exist?(File.expand_path("../config/doooit"))
+	    	@@config = File.expand_path("../config/doooit")  
 	    else
-	      puts "There's no configuration! please create one at ~/.doit"
+	      puts "There's no configuration! please create one at ~/.doooit"
 	      exit!
 	  	end
 	    source @@config
